99ee9baff5a918b26cd8e89d11c8fa32e4536927
OgnaigO <127579416+OgnaigO@users.noreply.github.com>
2026-05-31 10:00:25 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementById('toggleActivities');
-const activitiesList = document.getElementById('activities');
-
-toggleButton.addEventListener('click', () => {
-    activitiesList.classList.toggle('hidden');
-});
